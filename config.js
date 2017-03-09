23f3f38aca1adf7d7afec2d79752b5217faf32c4
Moraru Ion <morarou.y@gmail.com>
2017-03-09 17:44:38 +0100

    fix

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
-const lol = process.env.USER_KEY;
+const dotenv = require('dotenv').config();
 module.exports = {
   'port': process.env.PORT || 3000,
-  'dbUrl': "mongodb://kickass:kickass2017@ds123930.mlab.com:23930/kickass"
+  'dbUrl': `mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@ds123930.mlab.com:23930/kickass`
 }
-console.log(lol);
+
+
+//console.log(process.env.USER_DB);
+//console.log(process.env.PASSWORD_DB);
+//console.log("mongodb://"+process.env.USER_DB+":"+process.env.PASSWORD_DB+"@ds123930.mlab.com:23930/kickass");
+//console.log(`mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@ds123930.mlab.com:23930/kickass`);
