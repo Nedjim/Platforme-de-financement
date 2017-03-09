ae2f16d9fb48acee30b19d62c7252cb649b47cfd
Moraru Ion <morarou.y@gmail.com>
2017-03-09 16:53:24 +0100

    les  variable  mot d epasse

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+const lol = process.env.USER_KEY;
 module.exports = {
   'port': process.env.PORT || 3000,
   'dbUrl': "mongodb://kickass:kickass2017@ds123930.mlab.com:23930/kickass"
 }
+console.log(lol);
