5d0eb633d603e6da512ab0850d1e0c9f7815bd9d
jeffpash <jeffypash@gmail.com>
2017-03-15 19:06:40 +0100

    first team commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,96 +1,197 @@
 import React from 'react';
 import './Footer.scss';
+import './Footer.js';
+import img1 from '../images/tridelnik.jpg'
 export default class Footer extends React.Component {
     render() {
         return (
             <footer>
                 <div className="footer-wrap">
                     <div className="Footer">
-                        <h2><a href="">KickAssStarter</a></h2>
                         <div className="three">
-                          <div className="col1">
-                            <h3>financed</h3>
-                            <div className="cont">
-                              <img src="" alt=""/>
-                              <p>lorem paragraph text ljkjdhdolwiufiicjejlicukl</p>
+                            <div className="col1">
+                                <div>
+                                    <h3>Financé et enligne</h3>
+                                </div>
+                                <div className="cont">
+                                    <img src={img1} alt=""/>
+                                    <table>
+                                        <tr>
+                                            <th>Nom de projet:</th>
+                                            <td>blaaablaaa</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Description:</th>
+                                            <td>lorem paragraph text ljkjdhdolwiufiicjejlicukl</td>
+                                        </tr>
+                                        <tr>
+                                            <th>progrès:</th>
+                                            <td>
+                                                <progress value="50" max="100"></progress>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <button>FAIRE UN DON</button>
+                                    <a>
+                                        <i className="fa fa-heart" aria-hidden="true"></i>
+                                    </a>
+                                    <button>TOUT AFFICHER</button>
+                                </div>
                             </div>
-                          </div>
-                          <div className="col1"><h3>oncourse</h3></div>
-                          <div className="col1"><h3>latest</h3></div>
-                        </div>
+                            <hr className="vr"/>
 
-                        <hr/>
+                            <div className="col1">
+                                <div>
+                                    <h3>En cours de financement</h3>
+                                </div>
+                                <div className="cont">
+                                    <img src={img1} alt=""/>
+                                    <table>
+                                        <tr>
+                                            <th>Nom de projet:</th>
+                                            <td>blaaablaaa</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Description:</th>
+                                            <td>lorem paragraph text ljkjdhdolwiufiicjejlicukl</td>
+                                        </tr>
+                                        <tr>
+                                            <th>progrès:</th>
+                                            <td>
+                                                <progress value="20" max="100"></progress>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <button>FAIRE UN DON</button>
+                                    <a>
+                                        <i className="fa fa-heart" aria-hidden="true"></i>
+                                    </a>
+                                    <button>TOUT AFFICHER</button>
+                                </div>
+                            </div>
+                            <hr className="vr"/>
+
+                            <div className="col1">
+                                <div>
+                                    <h3>Projet recént</h3>
+                                </div>
+                                <div className="cont">
+                                    <img src={img1} alt=""/>
+                                    <table>
+                                        <tr>
+                                            <th>Nom de projet:</th>
+                                            <td>blaaablaaa</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Description:</th>
+                                            <td>lorem paragraph text ljkjdhdolwiufiicjejlicukl</td>
+                                        </tr>
+                                        <tr>
+                                            <th>progrès:</th>
+                                            <td>
+                                                <progress value="30" max="100"></progress>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <button>FAIRE UN DON</button>
+                                    <a>
+                                        <i className="fa fa-heart" aria-hidden="true"></i>
+                                    </a>
+                                    <button>TOUT AFFICHER</button>
+                                </div>
+                            </div>
+                        </div>
                         <div className="wrap">
+                            <div className="footer-text">
+                                <hr className="vr"/>
 
-                        <div className="footer-text">
-                            <div className="text1">
-                                <h3>Footer</h3>
-                                <ul>
-                                    <li>someText</li>
-                                    <li>someText</li>
-                                    <li>someText</li>
-                                </ul>
-                            </div>
-                            <div className="text1">
-                                <h3>Footer</h3>
-                                <ul>
-                                    <li>someText</li>
-                                    <li>someText</li>
-                                    <li>someText</li>
-                                </ul>
-                            </div>
-                            <div className="text1">
-                                <h3>Footer</h3>
-                                <ul>
-                                    <li>asomeText</li>
-                                    <li>someText</li>
-                                    <li>someText</li>
-                                </ul>
-                            </div>
-                        </div>
+                                <div className="text1">
+                                    <h3>Qui sommes-nous ?</h3>
+                                    <ul>
+                                        <li>
+                                            <a>-Les Partenaires</a>
+                                        </li>
+                                        <li>
+                                            <a>-L’équipe</a>
+                                        </li>
+                                        <li>
+                                            <a>-FAQ</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <hr className="vr"/>
+                                <div className="text1">
+                                    <h3>Footer</h3>
+                                    <ul>
+                                        <li>
+                                            <a>-Les Partenaires</a>
+                                        </li>
+                                        <li>
+                                            <a>-L’équipe</a>
+                                        </li>
+                                        <li>
+                                            <a>-FAQ</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <hr className="vr"/>
 
-                        <hr/>
-
-                        <div className="base">
-                            <div>
-                                <p><strong>KickAssStarter</strong><br/>&copy;2017</p>
+                                <div className="text1">
+                                    <h3>Contacter nous</h3>
+                                    <ul>
+                                        <li>
+                                            <a>-Les Partenaires</a>
+                                        </li>
+                                        <li>
+                                            <a>-L’équipe</a>
+                                        </li>
+                                        <li>
+                                            <a>-FAQ</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <hr className="vr"/>
                             </div>
-                            <div className="footer-icon">
-                                <ul className="footer-icon">
-                                    <li>
-                                        <a>
-                                            <i className=
-                                              "fa fa-facebook-official" aria-hidden="true"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a><i className="fa fa-twitter" aria-hidden="true"></i></a>
-                                    </li>
-                                    <li>
-                                        <a><i className="fa fa-instagram" aria-hidden="true"></i></a>
-                                    </li>
-                                    <li>
-                                        <a><i className="fa fa-pinterest" aria-hidden="true"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                              <a href="">
-
-                                <button>
-                                        <div>FR</div>
-                                        <div className="lang">
-                                          <ul>
-                                            <li>Francaise</li>
-                                            <li>English</li>
-                                          </ul>
-                                        </div>
-                                </button>
-                                </a>
+                            <hr/>
+                            <div className="base">
+                                <div>
+                                    <p>
+                                        <strong>
+                                            <a>KickAssStarter</a>
+                                        </strong><br/>&copy;2017</p>
+                                </div>
+                                <div className="footer-icon">
+                                    <ul className="footer-icon">
+                                        <li>
+                                            <a>
+                                                <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a>
+                                                <i className="fa fa-twitter" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a>
+                                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a>
+                                                <i className="fa fa-pinterest" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="lang">
+                                    <a>
+                                        <i className="fa fa-language" aria-hidden="true"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                  </div>
                 </div>
             </footer>
         )
