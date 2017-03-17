a72e2fbeaef78e72af460c49b1d887d33d3b8a2e
Sami <ssami.brahim@gmail.com>
2017-03-17 16:58:13 +0100

    ajoutdmontravail

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.scss';
 import './Footer.js';
-import img1 from '../images/trdelnik.jpg';
+import img1 from '../images/imgtest.jpg';
 export default class Footer extends React.Component {
     render() {
         return (
